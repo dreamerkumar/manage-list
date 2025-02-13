e82f90cbb860e0f92396e98a5272b81c0b728417
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-13 09:24:41 -0500

    Changed the top 10 list to be angular materialcard with a search box and a list of items.

--- src/app/top-ten-list/top-ten-list.component.ts
+++ src/app/top-ten-list/top-ten-list.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 interface ListItem {
   id: number;
@@ -10,7 +17,17 @@ interface ListItem {
 @Component({
   selector: 'app-top-ten-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule, 
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule
+  ],
   templateUrl: './top-ten-list.component.html',
   styleUrl: './top-ten-list.component.scss'
 })
